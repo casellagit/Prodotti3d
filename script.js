5d74f46be1a371abf15a115e867a7c0ef06b2eaa
casellagit <casella.enzo@gmail.com>
2025-02-27 07:46:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let videoTexture = null;
 // Attende che il custom element 'model-viewer' sia definito
 customElements.whenDefined('model-viewer').then(() => {
     // Crea una texture video utilizzando il file fiamma_transparente.webm
-    videoTexture = modelViewerAnimated.createVideoTexture("./fiamma_transparente.webm");
+    videoTexture = modelViewerAnimated.createVideoTexture("./fiamma_trasparente.webm");
 });
 
 // Aggiunge un listener per l'evento 'load' del model-viewer
